ae70b9b30b3933c03d76d8c53d4c21b5224db4be
qyccc <910845102@qq.com>
2023-12-19 16:05:39 +0800

    Update arguments.py

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -1107,6 +1107,8 @@ def _add_data_args(parser):
                        help='Sentencepiece tokenizer model.')
     group.add_argument('--tokenizer-name-or-path', type=str, default=None,
                        help='tokenizer model path for PretrainedFromHF.')
+    group.add_argument('--trust_remote_code', action='store_true',
+                       help='Whether trust remote code when using PretrainedFromHF.')
     group.add_argument('--data-impl', type=str, default='infer',
                        choices=['lazy', 'cached', 'mmap', 'infer'],
                        help='Implementation of indexed datasets.')
